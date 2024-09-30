1f18f01a43bc6b6908f1b32e4feb1ce2d1e72f8c
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-30 21:52:34 +0200

    fetch Spells

--- src/components/Spells/SpellsPage.tsx
+++ src/components/Spells/SpellsPage.tsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import type { Spell } from "../../@types/types";
+
 export function SpellsPage() {
-  return <h2>Spells</h2>;
+  const [spells, setSpells] = useState<Spell[]>([]);
+
+  useEffect(() => {
+    const fetchSpells = async () => {
+      try {
+        const response = await axios.get("https://hp-api.onrender.com/api/spells");
+        if (response.status === 200 && response.data) {
+          setSpells(response.data);
+          console.log(response.data);
+        } else {
+          throw new Error("Failed to fetch spells");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchSpells();
+  }, []);
+
+  return (
+    <>
+      <h2>Spells of Howgarts</h2>
+      <p>List of spells</p>
+      {spells.map((spell) => (
+        <p key={spell.id}>{spell.name}</p>
+      ))}
+    </>
+  );
 }
