dd4e5fe1223af28301a0902580e54c2eb31d4717
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-30 22:45:55 +0200

    create SpellCard component

---
+++ src/components/Spells/SpellCard.tsx
@@ -0,0 +1,23 @@
+import type { Spell } from "../../@types/types";
+
+type SpellProps = {
+  spell: Spell;
+};
+
+export default function SpellCard({ spell }: SpellProps) {
+  return (
+    <>
+      <article>
+        <div className="article-wrapper">
+          <figure>{<img src="https://picsum.photos/300/200" alt={"Illustration"} />}</figure>
+          <div className="article-body">
+            <h2>{spell.name}</h2>
+            <a href="url" className="read-more">
+              Read more <span className="sr-only">about this</span>
+            </a>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+}
