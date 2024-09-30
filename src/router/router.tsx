e2fde3c20843ead2382683b571f3f30c6d4c96ca
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-30 22:55:27 +0200

    fix: update path to files

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App";
-import { StudentsPage } from "../components/Students/StudentsPage";
-import { TeachersPage } from "../components/Teachers/TeachersPage";
-import { SpellsPage } from "../components/Spells/SpellsPage";
-import { HomePage } from "../components/Homepage/HomePage";
+import { StudentsPage } from "../pages/StudentsPage";
+import { TeachersPage } from "../pages/TeachersPage";
+import { SpellsPage } from "../pages/SpellsPage";
+import { HomePage } from "../pages/HomePage";
 
 const router = createBrowserRouter([
   {
