1ec02fc77828f2a9aa8b81604bbc7f53963c8ef2
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-30 21:24:25 +0200

    create RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import App from "./App.tsx";
-import "./index.css";
-
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import router from "./router/router";
 import "./index.css";
 
 // https://www.codingdeft.com/posts/react-18-typescript-error/
@@ -11,6 +10,6 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
