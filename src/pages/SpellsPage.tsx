8a85f020490da634a613e4fe551c3dcca8f13ebb
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 11:09:13 +0200

    use data from SpellsStore to display list of spells

--- src/pages/SpellsPage.tsx
+++ src/pages/SpellsPage.tsx
@@ -1,29 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-import type { Spell } from "../@types/types";
+import { store } from "../store/storeIndex";
 
 import SpellCard from "../components/SpellCard";
 
 export function SpellsPage() {
-  const [spells, setSpells] = useState<Spell[]>([]);
-
-  useEffect(() => {
-    const fetchSpells = async () => {
-      try {
-        const response = await axios.get("https://hp-api.onrender.com/api/spells");
-        if (response.status === 200 && response.data) {
-          setSpells(response.data);
-          console.log(response.data);
-        } else {
-          throw new Error("Failed to fetch spells");
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchSpells();
-  }, []);
+  const spells = store.spellsStore((state) => state.spells);
 
   return (
     <>
