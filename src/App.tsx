4d7cc19258fae8d248b9c1c06fbff0e679ca7462
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 11:09:53 +0200

    fetch data on app loading with useEffect()

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { Layout } from "./pages/Layout";
+import { store } from "./store/storeIndex";
 
 import "./App.css";
 
 function App() {
+  const fetchCharacters = store.charactersStore((state) => state.fetchCharacters);
+  const fetchSpells = store.spellsStore((state) => state.fetchSpells);
+
+  useEffect(() => {
+    const loadData = async () => {
+      await fetchCharacters();
+      await fetchSpells();
+    };
+    loadData();
+  }, [fetchCharacters, fetchSpells]);
   return (
     <>
       <Layout />
