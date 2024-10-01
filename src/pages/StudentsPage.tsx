d31f38ab1648b3ce49318fac95cd8ff65c1a0681
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 11:08:28 +0200

    use data from CharactersStore to display list of students

--- src/pages/StudentsPage.tsx
+++ src/pages/StudentsPage.tsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-import type { Character } from "../@types/types";
-
 import CharacterCard from "../components/CharacterCard";
+import { store } from "../store/storeIndex";
 
 export function StudentsPage() {
-  const [characters, setCharacters] = useState<Character[]>([]);
-
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        const response = await axios.get("https://hp-api.onrender.com/api/characters");
-        if (response.status === 200 && response.data) {
-          setCharacters(response.data);
-          console.log(response.data);
-        } else {
-          throw new Error("Failed to fetch characters");
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchCharacters();
-  }, []);
-
-  const students: Character[] = characters.filter(
-    (character) => character.hogwartsStudent === true,
-  );
+  const characters = store.charactersStore((state) => state.characters);
+  const students = characters.filter((character) => character.hogwartsStudent === true);
 
   return (
     <>
