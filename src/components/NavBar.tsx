fc7188314c57b58b3758a82fcc5fba4675319aa5
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 11:38:29 +0200

    update path to pages.json file

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,9 @@
 import { NavLink } from "react-router-dom";
 import Button from "./Button";
 
+import pages from "../data/pages.json";
+
 export function NavBar() {
-  const pages = [
-    {
-      id: 1,
-      title: "Students",
-      url: "/students",
-    },
-    {
-      id: 2,
-      title: "Teachers",
-      url: "/teachers",
-    },
-    {
-      id: 3,
-      title: "Other",
-      url: "/other",
-    },
-    {
-      id: 4,
-      title: "Spells",
-      url: "/spells",
-    },
-  ];
   return (
     <>
       {pages.map((page) => (
