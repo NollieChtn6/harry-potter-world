8f8f31816e4ac56033215e9ff794c5d65a83144c
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 14:24:28 +0200

    specify possible button types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,14 @@ import { SquareChevronRight } from "lucide-react";
 
 type ButtonProps = {
   text: string;
-  actionType: "nav" | "cta";
+  actionType: "nav" | "cta" | "formSubmit";
 };
 
 export default function Button({ text, actionType }: ButtonProps) {
   const buttonClass = actionType === "nav" ? "nav-btn" : "cta-btn";
+  const buttonType = actionType === "formSubmit" ? "submit" : "button";
   return (
-    <button className={buttonClass} type="button">
+    <button className={buttonClass} type={buttonType}>
       {text}
       {actionType === "cta" && <SquareChevronRight />}
     </button>
